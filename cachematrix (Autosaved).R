f6d8fb0e8586a44dbd2d1265f01ae8fe7854d36a
sizhuoli <sizhuo.li@outlook.com>
2017-10-28 15:35:04 +0800

    Add comments on the use of the function and the code
    Add comments on the use of the function and the code

--- cachematrix (Autosaved).R
+++ cachematrix (Autosaved).R
@@ -1,5 +1,7 @@
+## The 'makeCacheMatrix' function creates a special "matrix" that can cache its inverse.
 ## This function contains functions to:
 ## 1. set the value of the matrix
+## 2. get the value of the matrix
 ## 3. set the value of the inverse of the matrix
 ## 4. get the value of the inverse of the matrix
 
@@ -10,12 +12,14 @@ makeCacheMatrix <- function(x = matrix()) {
                 x <<- y
                 inv <<- NULL
          }
+        get <- function() x
         setinverse <- function(inverse) inv <<- inverse
         getinverse <- function() inv
         list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
         }
         
 
+## Function "cacheSolve" can be used to get the inverse of a square matrix quickly.
 ## If the inverse has already been calculated, then the function should retrieve the inverse from the cache.
 ## This function assumes that the matrix is always invertive.
 
